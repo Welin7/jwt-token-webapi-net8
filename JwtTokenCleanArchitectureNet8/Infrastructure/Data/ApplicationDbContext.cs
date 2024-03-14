f42ba0ce47dae9c21dfd8ba36c084f17e740860e
Welington Dias <33883532+Welin7@users.noreply.github.com>
2024-03-14 01:02:18 -0300

    Configuration ApplicationDbContext and generate migration.
    Create the class of context, serviceContainer to configuration of services include connection string. Generate the migration on infrastructure project.

---
+++ JwtTokenCleanArchitectureNet8/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)   
+    {
+    }
+
+    public DbSet<ApplicationUser> Users { get; set; }
+}
