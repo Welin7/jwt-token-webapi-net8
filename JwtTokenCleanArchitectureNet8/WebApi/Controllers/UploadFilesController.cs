3cdf5a0a9ce8113addc402f37b29a64841c1a084
Welington Dias <33883532+Welin7@users.noreply.github.com>
2024-03-27 23:42:55 -0300

    Create endpoint UploadFiles.
    Add UploadFiles controller to upload many files with authorize.

---
+++ JwtTokenCleanArchitectureNet8/WebApi/Controllers/UploadFilesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UploadFilesController : ControllerBase
+{
+    [Authorize]
+    [HttpPost("upload")]
+    public async Task<ActionResult> UploadFiles([FromForm] ICollection<IFormFile> files)
+    {
+        if (files == null || files.Count == 0)
+            return BadRequest("There are no valid files");
+
+        List<byte[]> data = new();
+
+        foreach (var formFile in files)
+        {
+            if (formFile.Length > 0)
+            {
+                using (var stream  = new MemoryStream()) 
+                {
+                    await formFile.CopyToAsync(stream);
+                    data.Add(stream.ToArray());
+                }
+            }
+        }
+
+        return File(data[0], files.FirstOrDefault().ContentType, "File.jpg");
+    }
+}
